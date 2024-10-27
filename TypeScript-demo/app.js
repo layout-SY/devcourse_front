75446d87e7a0f48e52e1c231f1f015924dd069cb
kdu <boycho2000@naver.com>
2024-10-27 22:36:45 +0900

    ts. interface를 통한 리턴 타입 개별 정의

--- TypeScript-demo/app.js
+++ TypeScript-demo/app.js
@@ -2,3 +2,46 @@ function logName(name) {
     console.log(name);
 }
 logName('cho');
+var student = {
+    name: 'cho',
+    age: 20,
+    grade: function () {
+        console.log('A');
+    },
+};
+student.name = 'cho seung yeon';
+student.age = 30;
+//데이터 타입 명시
+var stdId = 1111;
+var stdName = 'lee';
+var age = 20;
+var gender = 'male';
+var course = 'TypeScript';
+var completed = false;
+// 함수의 데이터 타입 명시(매개변수, 리턴타입)
+function Plus(a, b) {
+    // void는 리턴문이 없는 함수
+    //return a + b;
+}
+// 리턴 타입 개별 정의
+// function getInfo(id: number): {
+// 	stdId: number;
+// 	stdName: string;
+// 	age: number;
+// 	gender: string;
+// 	course: string;
+// 	completed: boolean;
+// } {
+// 	return {};
+// }
+function getInfo(id) {
+    return {
+        stdId: id,
+        stdName: 'lee',
+        age: 20,
+        gender: 'female',
+        course: 'typescript',
+        completed: true,
+    };
+}
+console.log(getInfo(5678));
--- TypeScript-demo/app.ts
+++ TypeScript-demo/app.ts
@@ -3,3 +3,61 @@ function logName(name: string) {
 }
 
 logName('cho');
+
+let student = {
+	name: 'cho',
+	age: 20,
+	grade: function () {
+		console.log('A');
+	},
+};
+
+student.name = 'cho seung yeon';
+student.age = 30;
+
+//데이터 타입 명시
+let stdId: number = 1111;
+let stdName: string = 'lee';
+let age: number = 20;
+let gender: string = 'male';
+let course: string = 'TypeScript';
+let completed: boolean = false;
+
+// 함수의 데이터 타입 명시(매개변수, 리턴타입)
+function Plus(a: number, b: number): void {
+	// void는 리턴문이 없는 함수
+	//return a + b;
+}
+
+// 리턴 타입 개별 정의
+// function getInfo(id: number): {
+// 	stdId: number;
+// 	stdName: string;
+// 	age: number;
+// 	gender: string;
+// 	course: string;
+// 	completed: boolean;
+// } {
+// 	return {};
+// }
+function getInfo(id: number): Student {
+	return {
+		stdId: id,
+		stdName: 'lee',
+		age: 20,
+		gender: 'female',
+		course: 'typescript',
+		completed: true,
+	};
+}
+
+interface Student {
+	stdId: number;
+	stdName: string;
+	age: number;
+	gender: string;
+	course: string;
+	completed: boolean;
+}
+
+console.log(getInfo(5678));
