3d18679a78aa4dec520b0b10dd306f4d1163876d
kdu <boycho2000@naver.com>
2024-10-30 14:37:39 +0900

    react 실습

---
+++ todolist/src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+// function App() {
+// 	return (
+// 		<div className="App-header">
+// 			<h1>Hello, 리액트 !!</h1>
+// 			<p>반갑습니다.</p>
+// 		</div>
+// 	);
+// }
+// function App() {
+// 	return React.createElement('div', null, 'Hello, 리액트', React.createElement('p', null, '반갑습니다'));
+// }
+
+function App() {
+	let name = '리액트';
+	const style = {
+		backgroundColor: '#FFFFFF',
+		color: 'black',
+		fontSize: '24px',
+		fontWeight: 'bold',
+		padding: '20px',
+	};
+
+	return (
+		<div className="container">
+			<h1 className="test">
+				Hello,
+				{name === '리액트' ? <h1>Yes</h1> : <h1>No</h1>}!!
+			</h1>
+			<p style={style}>반갑습니다.</p>
+			{/*주석문 작성 */}
+		</div>
+	);
+	// const port = undefined;
+	// return <div>{port || '포트를 설정하지 않았습니다.'}</div>;
+}
+export default App;
