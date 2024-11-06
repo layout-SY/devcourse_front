47450fc962a1e354e2951d5647be7259b49ffd1a
kdu <boycho2000@naver.com>
2024-11-07 01:30:56 +0900

    feat. firebase를 통한 로그인 구현

--- react-task-app/src/components/BoardList/BoardList.tsx
+++ react-task-app/src/components/BoardList/BoardList.tsx
@@ -1,9 +1,13 @@
 import React, { FC, useRef, useState } from 'react';
-import { useTypedSelector } from '../../hooks/redux';
+import { useTypedDispatch, useTypedSelector } from '../../hooks/redux';
 import SideForm from './SideForm/SideForm';
-import { FiPlusCircle } from 'react-icons/fi';
+import { FiLogIn, FiPlusCircle } from 'react-icons/fi';
 import { addButton, addSection, boardItem, boardItemActive, container, title } from './BoardList.css';
 import clsx from 'clsx';
+import { GoSignOut } from 'react-icons/go';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import app from '../../firebase';
+import { setUser } from '../../store/slices/userSlice';
 
 type TBoardListProps = {
 	activeBoardId: string;
@@ -14,12 +18,30 @@ const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId }) => 
 	const { boardArray } = useTypedSelector((state) => state.board);
 	const [isFormOpen, setIsFormOpen] = useState(false);
 	const inputRef = useRef<HTMLInputElement>(null);
+	const auth = getAuth(app);
+	const provider = new GoogleAuthProvider();
+	const dispatch = useTypedDispatch();
 
 	const handleClick = () => {
 		setIsFormOpen(!isFormOpen);
 		setTimeout(() => {
 			inputRef.current?.focus();
 		}, 0);
+	};
+
+	const handleLogin = () => {
+		signInWithPopup(auth, provider)
+			.then((userCredential) => {
+				console.log(`User ${userCredential}`);
+
+				dispatch(
+					setUser({
+						email: userCredential.user.email,
+						id: userCredential.user.uid,
+					})
+				);
+			})
+			.catch((e) => e);
 	};
 
 	return (
@@ -47,6 +69,9 @@ const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId }) => 
 				) : (
 					<FiPlusCircle className={addButton} onClick={handleClick} />
 				)}
+
+				<GoSignOut className={addButton} />
+				<FiLogIn className={addButton} onClick={handleLogin} />
 			</div>
 		</div>
 	);
---
+++ react-task-app/src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyDYxj5K4xl0ZAauN5KTDRUeyCWwKgXugS8',
+	authDomain: 'react-test-app-53ad0.firebaseapp.com',
+	projectId: 'react-test-app-53ad0',
+	storageBucket: 'react-test-app-53ad0.firebasestorage.app',
+	messagingSenderId: '1065055946858',
+	appId: '1:1065055946858:web:ef63d79b37310dbdd16213',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
