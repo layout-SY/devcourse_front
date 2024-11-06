5e7acbd23b4d70da20b0e9cc57ae3c492702ef01
kdu <boycho2000@naver.com>
2024-11-06 19:08:24 +0900

    feat. Modal 구현

--- react-task-app/src/store/slices/boardsSlice.ts
+++ react-task-app/src/store/slices/boardsSlice.ts
@@ -26,6 +26,12 @@ type TAddTaskAction = {
 	task: ITask;
 };
 
+type TDeleteTaskAction = {
+	boardId: string;
+	listId: string;
+	taskId: string;
+};
+
 const initialState: TBoardState = {
 	modalActive: false,
 	boardArray: [
@@ -108,6 +114,46 @@ const boardsSlice = createSlice({
 			});
 		},
 
+		updateTask: (state, { payload }: PayloadAction<TAddTaskAction>) => {
+			state.boardArray = state.boardArray.map((board) =>
+				board.boardId === payload.boardId
+					? {
+							...board,
+							lists: board.lists.map((list) => {
+								if (list.listId === payload.listId) {
+									return {
+										...list,
+										tasks: list.tasks.map((task) =>
+											task.taskId === payload.task.taskId ? payload.task : task
+										),
+									};
+								}
+								return list;
+							}),
+					  }
+					: board
+			);
+		},
+
+		deleteTask: (state, { payload }: PayloadAction<TDeleteTaskAction>) => {
+			state.boardArray = state.boardArray.map((board) =>
+				board.boardId === payload.boardId
+					? {
+							...board,
+							lists: board.lists.map((list) => {
+								if (list.listId === payload.listId) {
+									return {
+										...list,
+										tasks: list.tasks.filter((task) => task.taskId !== payload.taskId),
+									};
+								}
+								return list;
+							}),
+					  }
+					: board
+			);
+		},
+
 		deleteList: (state, { payload }: PayloadAction<TDeleteListAction>) => {
 			state.boardArray = state.boardArray.map((board) =>
 				board.boardId === payload.boardId
@@ -125,5 +171,5 @@ const boardsSlice = createSlice({
 	},
 });
 
-export const { addBoard, deleteList, setModalActive, addList, addTask } = boardsSlice.actions;
+export const { addBoard, deleteList, deleteTask, updateTask, setModalActive, addList, addTask } = boardsSlice.actions;
 export const boardReducer = boardsSlice.reducer;
