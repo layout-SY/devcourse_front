54e7fba09c4e965138b3942a7f70bc2b23a4f18f
kdu <boycho2000@naver.com>
2024-11-15 14:36:29 +0900

    11월 15일 강의

--- book-store/src/style/theme.ts
+++ book-store/src/style/theme.ts
@@ -2,7 +2,7 @@ export type ThemeName = 'light' | 'dark';
 export type colorKey = 'primary' | 'background' | 'secondary' | 'third' | 'border' | 'text';
 export type HeadingSize = 'large' | 'medium' | 'small';
 export type ButtonSize = 'large' | 'medium' | 'small';
-export type ButtonScheme = 'primary' | 'normal';
+export type ButtonScheme = 'primary' | 'normal' | 'like';
 export type LayoutWidth = 'large' | 'medium' | 'small';
 
 interface Theme {
@@ -88,6 +88,10 @@ export const light: Theme = {
 		normal: {
 			color: 'black',
 			backgroundColor: 'lightgrey',
+		},
+		like: {
+			color: 'white',
+			backgroundColor: 'coral',
 		},
 	},
 	borderRadius: {
